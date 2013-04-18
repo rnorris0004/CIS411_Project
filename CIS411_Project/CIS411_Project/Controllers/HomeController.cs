137d1ee1f630c152eb2a02e5cef3bf870d2d28b3
John Stinson <xerohour+git@gmail.com>
2013-04-18 10:37:51 -0400

    fixed namespace
    added comment in home controller

--- CIS411_Project/CIS411_Project/Controllers/HomeController.cs
+++ CIS411_Project/CIS411_Project/Controllers/HomeController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using CIS411_Project.Core.Models;
+//changed this
+using CIS411_Project;
 
 namespace CIS411_Project.Controllers
 {
