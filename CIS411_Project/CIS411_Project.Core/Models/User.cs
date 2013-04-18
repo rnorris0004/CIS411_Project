e0ffad822a74091fb4f8554d266d653588e847cb
rnorris0004 <rsnorris84@gmail.com>
2013-04-17 23:34:18 -0400

    Models complete, using error on Home Controller

---
+++ CIS411_Project/CIS411_Project.Core/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CIS411_Project.Core.Models
+{
+    public class User
+    {
+        public int USER_ID { get; set; }
+
+        public string USER_FNAME { get; set; }
+
+        public string USER_LNAME { get; set; }
+
+        public string USER_DISPLAYNAME { get; set; }
+
+        public int REPUTATION { get; set; }
+
+        public string EMAIL { get; set; }
+
+        public DateTime CREATED_TIMESTAMP { get; set; }
+    }
+}
--- CIS411_Project/CIS411_Project/Controllers/HomeController.cs
+++ CIS411_Project/CIS411_Project/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CIS411_Project.Core.Models;
 
 namespace CIS411_Project.Controllers
 {
