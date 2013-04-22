69c162f269659c3d83b52564194a8e09842757c4
rnorris0004 <rsnorris84@gmail.com>
2013-04-22 16:10:07 -0400

    Add EF & Repo

--- CIS411_Project/CIS411_Project.dal/Repositories/BookRepo.cs
+++ CIS411_Project/CIS411_Project.dal/Repositories/BookRepo.cs
@@ -8,45 +8,45 @@ using System.Threading.Tasks;
 
 namespace CIS411_Project.dal.Repositories
 {
-         public class BookRepo : IRepository<BOOK>
+    public class BookRepo : IRepository<BOOK>
+    {
+        private BooksDBEntities _context = null;
+
+        public BookRepo()
         {
-            private BooksDBEntities _context = null;
+            _context = new BooksDBEntities();
+        }
+        public BOOK getById(BOOK object2add)
+        {
+            return _context.BOOKs.Find(object2add.BOOK_ID);
+        }
 
-            public BookRepo()
-            {
-                _context = new BooksDBEntities();
-            }
-            public BOOK getById(BOOK object2add)
-            {
-                return _context.BOOKs.Find(object2add.BOOK_ID);
-            }
+        public BOOK[] getAll()
+        {
+            return _context.BOOKs.ToArray();
+        }
 
-            public BOOK[] getAll()
-            {
-                return _context.BOOKs.ToArray();
-            }
+        public void add(BOOK book2add)
+        {
+            _context.BOOKs.Add(book2add);
+            _context.SaveChanges();
+        }
 
-            public void add(BOOK book2add)
-            {
-                _context.BOOKs.Add(book2add);
-                _context.SaveChanges();
-            }
+        public void update(BOOK book2update)
+        {
+            _context.Entry(book2update).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
 
-            public void update(BOOK book2update)
-            {
-                _context.Entry(book2update).State = EntityState.Modified;
-                _context.SaveChanges();
-            }
+        public void remove(BOOK book2remove)
+        {
+            _context.BOOKs.Remove(book2remove);
+            _context.SaveChanges();
+        }
 
-            public void remove(BOOK book2remove)
-            {
-                _context.BOOKs.Remove(book2remove);
-                _context.SaveChanges();
-            }
-
-            public IQueryable<BOOK> query(System.Linq.Expressions.Expression<Func<BOOK, bool>> filter)
-            {
-                return _context.BOOKs.Where(filter);
-            }
+        public IQueryable<BOOK> query(System.Linq.Expressions.Expression<Func<BOOK, bool>> filter)
+        {
+            return _context.BOOKs.Where(filter);
+        }
     }
 }
--- CIS411_Project/CIS411_Project.dal/Repositories/IRepository.cs
+++ CIS411_Project/CIS411_Project.dal/Repositories/IRepository.cs
@@ -8,14 +8,14 @@ using System.Linq.Expressions;
 namespace Repository.dal
 {
 
-       interface IRepository<T>
-        {
-            T getById(T object2Add);
-            T[] getAll();
-            void add(T object2Add);
-            void update(T object2Update);
-            void remove(T object2Remove);
-            IQueryable<T> query(Expression<Func<T, bool>> filter);
-        }
-    
+    interface IRepository<T>
+    {
+        T getById(T object2Add);
+        T[] getAll();
+        void add(T object2Add);
+        void update(T object2Update);
+        void remove(T object2Remove);
+        IQueryable<T> query(Expression<Func<T, bool>> filter);
+    }
+
 }
